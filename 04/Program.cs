4085f8150b82d6ea7e5d7cc49b209f859ce99a48
Tobias Dersén <tobberoth@gmail.com>
2023-11-21 18:20:31 +0100

    Day 4 done

---
+++ 04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+var secret = "ckczppom";
+Console.WriteLine(FindSmallestAddition(secret, "00000"));
+Console.WriteLine(FindSmallestAddition(secret, "000000"));
+
+int FindSmallestAddition(string secret, string start) {
+  using var md5 = MD5.Create();
+  for (int i = 0; i < 10000000; i++) {
+    var result = CreateMD5(md5, secret + i.ToString());
+    if (result.StartsWith(start))
+      return i;
+  }
+  return 0;
+}
+
+string CreateMD5(MD5 md5, string input) {
+  byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+  byte[] hashBytes = md5.ComputeHash(inputBytes);
+  return Convert.ToHexString(hashBytes);
+}
