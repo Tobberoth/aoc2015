c83f902a55f8867aa3cb225386bf3301f14a6d23
Tobias Dersén <tobberoth@gmail.com>
2023-11-22 19:26:36 +0100

    Day 10 done

---
+++ 10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+var word = "1113222113";
+var timesToRepeat = 50;
+for (var i = 0; i < timesToRepeat; i++)
+  word = LookAndSay(word);
+Console.WriteLine(word.Length);
+
+static string LookAndSay(string input) {
+  StringBuilder output = new();
+  char currentChar = ' ';
+  int count = 0;
+  foreach (char c in input) {
+    if (currentChar == ' ')
+      currentChar = c;
+    if (c != currentChar) {
+      output.Append($"{count}{currentChar}");
+      count = 0;
+      currentChar = c;
+    }
+    count++;
+  }
+  output.Append($"{count}{currentChar}");
+  return output.ToString();
+}
